5b7affe791b62862c45a0c1deff1cdd33eba69e8
Alex MacArthur <alex.macarthur@daveramsey.com>
2018-01-18 18:25:19 -0600

    Fix invalid JS.

--- src/components/BaseHelmet.js
+++ src/components/BaseHelmet.js
@@ -13,8 +13,8 @@ class BaseHelmet extends React.Component {
           <meta name="description" content={config.siteMetadata.description} />
           <meta name="keywords" content="freelance, web development, web developer, WordPress, javascript" />
           <meta name="author" content={config.siteMetadata.author} />
-          <meta property="fb:admins" content="502371334" /> 
-          <meta property="og:type" content="website" /> 
+          <meta property="fb:admins" content="502371334" />
+          <meta property="og:type" content="website" />
           <meta property="og:title" content={config.siteMetadata.title} />
           <meta property="og:description" content={config.siteMetadata.description} />
           <meta property="og:image" content="https://raw.githubusercontent.com/alexmacarthur/macarthur.me/127ca1d9/static/open-graph.jpg" />
@@ -32,7 +32,7 @@ class BaseHelmet extends React.Component {
                 },
                   "image" : "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAASZAAAAJGI5NDJiMTg5LWI3YmYtNDg1OC1hNjE1LTg1ZmMzNzFjYmJhMQ.jpg",
                 "email": "alex@macarthur.me",
-                "description": ""I'm a front-end web developer in Nashville, spending most of my time in WordPress and JavaScript. Hire me for your next project!",
+                "description": "I'm a front-end web developer in Nashville, spending most of my time in WordPress and JavaScript. Hire me for your next project!",
                 "name": "Alex MacArthur / Nashville Web Developer",
                 "sameAs" : [ "http://www.twitter.com/amacarthur", "https://www.linkedin.com/in/alexmacarthur", "https://github.com/alexmacarthur"],
                 "telephone" : "507-476-1414",
