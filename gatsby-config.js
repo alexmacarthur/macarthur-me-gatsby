076ede413aabeb8f72999acb4f040cfad9f080e7
Alex MacArthur <alex.macarthur@daveramsey.com>
2019-06-19 21:27:31 -0500

    Remove lazy loading.

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,8 +57,7 @@ module.exports = {
           },
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
-          "gatsby-remark-smartypants", 
-          "gatsby-remark-lazy-load"
+          "gatsby-remark-smartypants"
         ]
       }
     },
