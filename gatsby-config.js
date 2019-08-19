c45d187a3cacaa7b3043b949ba4441060e4adda8
Alex MacArthur <alex.macarthur@daveramsey.com>
2019-08-19 17:51:06 -0500

    trying to fix tweet.

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,12 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`
             }
           },
+          {
+            resolve: "@weknow/gatsby-remark-twitter",
+            options: {
+              debug: true
+            }
+          },
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
           "gatsby-remark-smartypants"
